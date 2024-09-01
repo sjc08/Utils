3a50031ee0b20b33795e3356cf198ff8e3725036
AlexSJC <cn_sjc@outlook.com>
2024-09-01 21:54:11 +0800

    docs: add comments

--- Utils/Tryer.cs
+++ Utils/Tryer.cs
@@ -4,6 +4,11 @@ namespace Asjc.Utils
 {
     public static class Tryer
     {
+        /// <summary>
+        /// Trys to execute <paramref name="action"/>.
+        /// </summary>
+        /// <param name="action">The delegate to be executed.</param>
+        /// <returns>A <see langword="bool"/> indicating whether no exception has occurred.</returns>
         public static bool Try(Action action)
         {
             try
@@ -17,6 +22,12 @@ namespace Asjc.Utils
             }
         }
 
+        /// <summary>
+        /// Trys to execute <paramref name="func"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the return value of <paramref name="func"/>.</typeparam>
+        /// <param name="func">The delegate to be executed.</param>
+        /// <returns>The return value of <paramref name="func"/> if no exception has occurred; otherwise, <see langword="default"/>.</returns>
         public static T? Try<T>(Func<T> func)
         {
             try
@@ -29,6 +40,13 @@ namespace Asjc.Utils
             }
         }
 
+        /// <summary>
+        /// Trys to execute <paramref name="func"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the return value of <paramref name="func"/>.</typeparam>
+        /// <param name="func">The delegate to be executed.</param>
+        /// <param name="result">The return value of <paramref name="func"/> if no exception has occurred; otherwise, <see langword="default"/>.</param>
+        /// <returns>A <see langword="bool"/> indicating whether no exception has occurred.</returns>
 #if NETSTANDARD2_1 || NET8_0
         public static bool Try<T>(Func<T> func, [MaybeNullWhen(false)] out T result)
 #else
