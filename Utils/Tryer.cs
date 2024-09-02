52f3fd2aac98761764d395e1c84c4f5f63cf9ac5
AlexSJC <cn_sjc@outlook.com>
2024-09-02 22:24:16 +0800

    feat: update Tryer

--- Utils/Tryer.cs
+++ Utils/Tryer.cs
@@ -23,6 +23,26 @@ namespace Asjc.Utils
         }
 
         /// <summary>
+        /// Trys to execute <paramref name="action"/>.
+        /// </summary>
+        /// <param name="action">The delegate to be executed.</param>
+        /// <param name="onError">The delegate to be called when an exception occurs.</param>
+        /// <returns>A <see langword="bool"/> indicating whether no exception has occurred.</returns>
+        public static bool Try(Action action, Action<Exception> onError)
+        {
+            try
+            {
+                action();
+                return true;
+            }
+            catch (Exception e)
+            {
+                onError(e);
+                return false;
+            }
+        }
+
+        /// <summary>
         /// Trys to execute <paramref name="func"/>.
         /// </summary>
         /// <typeparam name="T">The type of the return value of <paramref name="func"/>.</typeparam>
@@ -37,6 +57,45 @@ namespace Asjc.Utils
             catch
             {
                 return default;
+            }
+        }
+
+        /// <summary>
+        /// Trys to execute <paramref name="func"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the return value of <paramref name="func"/>.</typeparam>
+        /// <param name="func">The delegate to be executed.</param>
+        /// <param name="onError">The delegate to be called when an exception occurs.</param>
+        /// <returns>The return value of <paramref name="func"/> if no exception has occurred; otherwise, <see langword="default"/>.</returns>
+        public static T? Try<T>(Func<T> func, Action<Exception> onError)
+        {
+            try
+            {
+                return func();
+            }
+            catch (Exception e)
+            {
+                onError(e);
+                return default;
+            }
+        }
+
+        /// <summary>
+        /// Trys to execute <paramref name="func"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the return value of <paramref name="func"/>.</typeparam>
+        /// <param name="func">The delegate to be executed.</param>
+        /// <param name="onError">The delegate to be called when an exception occurs.</param>
+        /// <returns>The return value of <paramref name="func"/> if no exception has occurred; otherwise, the return value of <paramref name="onError"/>.</returns>
+        public static T? Try<T>(Func<T> func, Func<Exception, T?> onError)
+        {
+            try
+            {
+                return func();
+            }
+            catch (Exception e)
+            {
+                return onError(e);
             }
         }
 
@@ -61,6 +120,59 @@ namespace Asjc.Utils
             catch
             {
                 result = default;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Trys to execute <paramref name="func"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the return value of <paramref name="func"/>.</typeparam>
+        /// <param name="func">The delegate to be executed.</param>
+        /// <param name="onError">The delegate to be called when an exception occurs.</param>
+        /// <param name="result">The return value of <paramref name="func"/> if no exception has occurred; otherwise, <see langword="default"/>.</param>
+        /// <returns>A <see langword="bool"/> indicating whether no exception has occurred.</returns>
+#if NETSTANDARD2_1 || NET8_0
+        public static bool Try<T>(Func<T> func, Action<Exception> onError, [MaybeNullWhen(false)] out T result)
+#else
+        public static bool Try<T>(Func<T> func, Action<Exception> onError, out T? result)
+#endif
+        {
+            try
+            {
+                result = func();
+                return true;
+            }
+            catch (Exception e)
+            {
+                onError(e);
+                result = default;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Trys to execute <paramref name="func"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the return value of <paramref name="func"/>.</typeparam>
+        /// <param name="func">The delegate to be executed.</param>
+        /// <param name="onError">The delegate to be called when an exception occurs.</param>
+        /// <param name="result">The return value of <paramref name="func"/> if no exception has occurred; otherwise, the return value of <paramref name="onError"/>.</param>
+        /// <returns>A <see langword="bool"/> indicating whether no exception has occurred.</returns>
+#if NETSTANDARD2_1 || NET8_0
+        public static bool Try<T>(Func<T> func, Func<Exception, T?> onError, [MaybeNullWhen(false)] out T result)
+#else
+        public static bool Try<T>(Func<T> func, Func<Exception, T?> onError, out T? result)
+#endif
+        {
+            try
+            {
+                result = func();
+                return true;
+            }
+            catch (Exception e)
+            {
+                result = onError(e);
                 return false;
             }
         }
